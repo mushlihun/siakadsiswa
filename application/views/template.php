54b9240318bc96bdbe1d37c7cb067e1b60f10902
MUSHLIHUN <mushlihun@asiapay.com>
2023-07-12 14:36:26 +0700

    update template

--- application/views/template.php
+++ application/views/template.php
@@ -236,9 +236,9 @@
 
   <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.0
+      <b>Version</b> 1.0.0
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    <strong>Copyright &copy; 2023 <a href="https://mushlihun.github.io">List Creative</a>.</strong> All rights
     reserved.
   </footer>
 
